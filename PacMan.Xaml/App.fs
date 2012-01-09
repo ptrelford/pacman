f01c8697fbddb0d4099565eecc0c80564a26d3f2
Moon <Moon@Moon-PC>
2012-01-09 08:23:40 +0000

    Added XAML/Silverlight version

---
+++ PacMan.Xaml/App.fs
@@ -0,0 +1,8 @@
+﻿namespace PacMan
+
+open System.Windows
+open System.Windows.Controls
+
+type App() as this = 
+    inherit Application()
+    do  this.Startup.AddHandler(fun o e -> this.RootVisual <- GameControl())
