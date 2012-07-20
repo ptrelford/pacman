4e07c9bfd447fdfc47bd7ee5dd4e10a404095757
ptrelford <ptrelford@localhost>
2012-07-20 08:19:25 +0100

    Centered & scaled game canvas on Silverlight C# version

--- PacMan.Xaml/MainControl.xaml.cs
+++ PacMan.Xaml/MainControl.xaml.cs
@@ -7,17 +7,29 @@ namespace PacMan.App
 {
     public partial class MainControl : UserControl
     {
+        ScaleTransform _transform;
+
         public MainControl()
         {
             InitializeComponent();
 
             var canvas = Main.CreateCanvas();
-            this.Width = canvas.Width;
-            this.Height = canvas.Height;
 
             Main.StartGame(this, canvas);
 
-            this.Content = canvas;                       
+            _transform = new ScaleTransform { 
+                CenterX = canvas.Width/2, 
+                CenterY = canvas.Height/2, 
+                ScaleX = 2.0, 
+                ScaleY = 2.0 };
+            canvas.RenderTransform = _transform;
+
+            this.LayoutRoot.Children.Add(canvas);
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            return base.MeasureOverride(availableSize);
         }
     }
 }
