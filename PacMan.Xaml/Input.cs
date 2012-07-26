0fbbf3331e11967643e5a4b9936c59fda96faa2a
ptrelford <ptrelford@localhost>
2012-07-26 08:15:58 +0100

    WP7 - move Pacman with accelerometer

--- PacMan.Xaml/Input.cs
+++ PacMan.Xaml/Input.cs
@@ -3,37 +3,84 @@ using Windows.System;
 using Key = Windows.System.VirtualKey;
 #else
 using System.Windows.Input;
+#if WINDOWS_PHONE
+using Microsoft.Devices.Sensors;
+using Microsoft.Xna.Framework;
+#endif 
 #endif
 
 namespace PacMan.App
 {
     public class Input : IInput
     {
+        #if WINDOWS_PHONE
+        Accelerometer acc;
+        float x;
+        float y;
+        #endif
+
         private readonly Keys _keys;
 
         internal Input(Keys keys)
         {
             _keys = keys;
+            #if WINDOWS_PHONE
+            acc = new Accelerometer();
+            acc.CurrentValueChanged += (s, e) =>
+                {
+                    x = e.SensorReading.Acceleration.X;
+                    y = e.SensorReading.Acceleration.Z;
+                };
+            acc.Start();
+            #endif
         }
 
         public bool IsDown
         {
-            get { return _keys.IsKeyDown(Key.Down); }
+            get 
+            { 
+#if WINDOWS_PHONE
+                return y > 0;
+#else
+                return _keys.IsKeyDown(Key.Down); 
+#endif
+            }
         }
 
         public bool IsLeft
         {
-            get { return _keys.IsKeyDown(Key.Left); }
+            get 
+            {
+#if WINDOWS_PHONE
+                return x < 0;
+#else
+                return _keys.IsKeyDown(Key.Left);             
+#endif
+            }
         }
 
         public bool IsRight
         {
-            get { return _keys.IsKeyDown(Key.Right); }
+            get
+            {
+#if WINDOWS_PHONE
+                return x > 0;
+#else
+                return _keys.IsKeyDown(Key.Right);             
+#endif
+            }
         }
 
         public bool IsUp
         {
-            get { return _keys.IsKeyDown(Key.Up); }
+            get
+            {
+#if WINDOWS_PHONE
+                return y < 0;
+#else
+                return _keys.IsKeyDown(Key.Up);             
+#endif
+            }
         }
     }
 }
