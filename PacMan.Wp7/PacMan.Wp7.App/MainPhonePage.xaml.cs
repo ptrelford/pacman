b2842e99167f669038e855f930ba408b03ce0114
ptrelford <ptrelford@localhost>
2012-07-26 07:48:38 +0100

    WP7 scale game canvas to screen size

--- PacMan.Wp7/PacMan.Wp7.App/MainPhonePage.xaml.cs
+++ PacMan.Wp7/PacMan.Wp7.App/MainPhonePage.xaml.cs
@@ -15,14 +15,44 @@ namespace PacMan.App
 {
     public partial class MainPhonePage : PhoneApplicationPage
     {
+        ScaleTransform _transform;
+        double _width;
+        double _height;
+
         // Constructor
         public MainPhonePage()
         {
             InitializeComponent();
-            
+         
             var canvas = Main.CreateCanvas();
+            _width = canvas.Width;
+            _height = canvas.Height;
+
+            _transform = new ScaleTransform
+            {
+                CenterX = _width / 2,
+                CenterY = _height / 2
+            };
+            canvas.RenderTransform = _transform;
+
             this.ContentPanel.Children.Add(canvas);
             Main.StartGame(this, canvas);     
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var scale = 1.0;
+            while ((_width * scale) < availableSize.Width &&
+                   (_height * scale) < availableSize.Height)
+                scale += 0.5;
+            scale = scale - 0.5;
+            if (scale < 1.0) scale = 1.0;
+            if (_transform.ScaleX != scale)
+            {
+                _transform.ScaleX = scale;
+                _transform.ScaleY = scale;
+            }
+            return base.MeasureOverride(availableSize);
         }
     }
 }
